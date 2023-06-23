e8ff9726c8b23ec815d65732864dee1d5c55d613
Isaac-a95 <isaac.alonso.a.95@gmail.com>
2023-06-23 15:43:30 +0100

    Add missing file

---
+++ pymiles/scripts/__init__.py
@@ -0,0 +1,11 @@
+import logging
+
+logger = logging.getLogger('pymiles')
+logger.setLevel(logging.DEBUG)
+
+for existing_handler in list(logger.handlers):
+    logger.removeHandler(existing_handler)
+
+ch = logging.StreamHandler()
+ch.setFormatter( logging.Formatter('%(name)s: %(message)s') )
+logger.addHandler(ch)
